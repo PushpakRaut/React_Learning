6f2ff64102593fbba43c2a034554c0ef5b6f8cce
Pushpak Raut <pushpakraut123@gmail.com>
2024-05-18 17:05:22 +0530

    React Learning

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react'
+// import ErrorExample from './tutorial/1-useState/setup/1-error-example'
+// import UsestateBasicExample from './tutorial/1-useState/setup/2-useState-basics'
+// import UseStateArray from './tutorial/1-useState/setup/3-useState-array'
+// import UseStateObject from './tutorial/1-useState/setup/4-useState-object'
+// import UseStateCounter from './tutorial/1-useState/setup/5-useState-counter'
+// import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics'
+// import UseEffectCleanUp from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+// import UseEffectFetchData from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+// import UseConditionalMultiple from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+// import UseConditionalShortCircuit from './tutorial/3-conditional-rendering/setup/2-short-circuit'
+// import UseConditionalShowHide from './tutorial/3-conditional-rendering/setup/3-show-hide'
+// import FormsControlledInputs from './tutorial/4-forms/setup/1-controlled-inputs'
+// import FormsMultipleInputs from './tutorial/4-forms/setup/2-multiple-inputs'
+// import UseRefBasics from './tutorial/5-useRef/setup/1-useRef-basics'
+// import UseReducerIndex from './tutorial/6-useReducer/setup/index'
+// import PropDrilling from './tutorial/7-prop-drilling/setup/1-prop-drilling'
+// import ContextAPI from './tutorial/8-useContext/setup/1-context-api'
+// import FetchExample from './tutorial/9-custom-hooks/setup/1-fetch-example'
+// import PropsTypes from './tutorial/10-prop-types/setup/index'
+// import ReactRouter from './tutorial/11-react-router/setup'
+import Memo from './tutorial/12-memo-useMemo-useCallback/setup/index'
+function App() {
+  return (
+    <div className='container'>
+      {/* <h2>advanced react practise app</h2> */}
+      {/* <ErrorExample /> */}
+      {/* <UsestateBasicExample /> */}
+      {/* <UseStateArray /> */}
+      {/* <UseStateObject /> */}
+      {/* <UseStateCounter /> */}
+      {/* <UseEffectBasics /> */}
+      {/* <UseEffectCleanUp /> */}
+      {/* <UseEffectFetchData /> */}
+      {/* <UseConditionalMultiple /> */}
+      {/* <UseConditionalShortCircuit /> */}
+      {/* <UseConditionalShowHide /> */}
+      {/* <FormsControlledInputs /> */}
+      {/* <FormsMultipleInputs /> */}
+      {/* <UseRefBasics /> */}
+      {/* <UseReducerIndex /> */}
+      {/* <PropDrilling /> */}
+      {/* <ContextAPI /> */}
+      {/* <FetchExample /> */}
+      {/* <PropsTypes /> */}
+      {/* <ReactRouter /> */}
+      <Memo />
+    </div>
+  )
+}
+
+export default App
